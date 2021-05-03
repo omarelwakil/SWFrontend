52c5f0d6b9ea2446e72d96126140d0c63fd16f81
Omar Elwakil <omar.tarek@klenka.com>
2021-05-03 06:23:18 +0200

    Reverts About Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import GuidelinesPage from './pages/GuidelinesPage';
 import SignUpPage from './pages/SignUpPage';
-import AboutPage from './pages/AboutPage'
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/help/guidelines" component={GuidelinesPage} />
         <Route exact path="/sign-up" component={SignUpPage} />
-        <Route exact path="/About" component={AboutPage} />
         {/* <Route exact path="/notfound" component={404_Errorpage} />
         <Redirect to="/notfound" /> */}
       </Switch>
