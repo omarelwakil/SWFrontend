78767af25521249c960b9d0580190cf98b793ab4
Omar Elwakil <omar.tarek@klenka.com>
2021-05-08 19:32:49 +0200

    Fixed a bug in SettingsPage

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -4,13 +4,22 @@ import { BrowserRouter } from 'react-router-dom';
 import UserlessNavigationBar from '../components/user/UserlessNavigationBar';
 import UserSettings from '../components/user/UserSettings';
 import Footer from '../components/static/Footer';
+import Navbar from '../components/Trending/Navbar/Navbar';
 
 function SettingsPage() {
+    const dataToSend = [
+        { title: "Personal Information", path: "/help/terms", selected: true }
+    ];
+    const position = "position-static";
     return (
         <BrowserRouter>
             <div>
                 <title>Account settings | Flickr</title>
                 <UserlessNavigationBar />
+                <div className="bg-white ps-5">
+                    <h1 className="header-title mb-0">Policies &amp; Guidelines</h1>
+                </div>
+                <Navbar items={dataToSend} position={position} />
                 <UserSettings />
                 <Footer />
             </div>
