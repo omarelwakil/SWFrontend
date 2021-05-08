106ff2315af5ef70e76aa42a47d85f5c1d513896
Omar Elwakil <omar.tarek@klenka.com>
2021-05-08 19:56:50 +0200

    Fixed override classes on all pages

--- src/components/static/Explore.js
+++ src/components/static/Explore.js
@@ -1,44 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 import './Explore.css';
 class Explore extends Component {
-  
+
   state = {
     pictures: []
   }
 
-  componentDidMount () {
+  componentDidMount() {
     axios.defaults.baseURL = 'https://5ef8aca7-aab5-4b26-97c0-8b913e9ef7ec.mock.pstmn.io';
     axios.get('/photo/explore')
-    .then(response => {
-      const updatedPictures = response.data.photos.map(picture => {
-        return {
-          ...picture,
-        className: 'myimg'
-      }
+      .then(response => {
+        const updatedPictures = response.data.photos.map(picture => {
+          return {
+            ...picture,
+            className: 'myimg'
+          }
+        });
+        this.setState({ pictures: updatedPictures });
       });
-      this.setState({pictures: updatedPictures});
-    });
   }
-  
-  render () {
-    
-    const pictures = this.state.pictures.map( (picture, index) => {
-      return  <div key={index} className="col-lg-3">
-                <img className ={picture.className} src={picture.url} alt={picture.title}/>
-                <div className="hide">  
-                  <h2 className="picTitle fw-bold"> {picture.title} </h2>
-                  <h3 className="creator inline fw-bold"> By: {picture.creator.firstName} {picture.creator.lastName} </h3>
-                  <button type="button" className="btn btn-light"> <i className="far fa-star inline">   </i> {picture.favouritesNum}  </button>
-                  <button type="button" className="btn btn-light"> <i className="far fa-comment inline"></i> {picture.commentsNum}    </button>
-                </div>
-              </div>;
+
+  render() {
+
+    const pictures = this.state.pictures.map((picture, index) => {
+      return <div key={index} className="col-lg-3">
+        <img className={picture.className} src={picture.url} alt={picture.title} />
+        <div className="hide">
+          <h2 className="picTitle fw-bold"> {picture.title} </h2>
+          <h3 className="creator inline fw-bold"> By: {picture.creator.firstName} {picture.creator.lastName} </h3>
+          <button type="button" className="btn btn-light"> <i className="far fa-star inline">   </i> {picture.favouritesNum}  </button>
+          <button type="button" className="btn btn-light"> <i className="far fa-comment inline"></i> {picture.commentsNum}    </button>
+        </div>
+      </div>;
     });
 
     return (
-      <div id="content">
-        <div className="main">
+      <div id="content" className="pt-3">
+        <div className="main pt-5">
           <div className="container title-row">
             <div className="row">
               <h3 className="col-sm Explore-Title inline">Explore</h3>
--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -4,14 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import UserlessNaviagationBar from '../components/user/UserlessNavigationBar';
 import Explore from '../components/static/Explore';
 import Footer from '../components/static/Footer';
+import Navbar from '../components/Trending/Navbar/Navbar'
 
 function ExplorePage() {
+
+    const dataToSend = [
+        { title: "Explore", path: "/explore", selected: true },
+        { title: "Trending", path: "/photos/tags", selected: false }
+    ];
     return (
         <BrowserRouter>
             <div>
                 <title>Explore | Flickr</title>
                 <UserlessNaviagationBar />
-                {/* We will add Explore Trending Events Nabigation Bar */}
+                <Navbar items={dataToSend} />
                 <Explore />
                 <Footer />
             </div>
