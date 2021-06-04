b96329459497a8bbdda88e27fdff29929575b438
Omar Elwakil <omartelwakil@gmail.com>
2021-06-04 08:03:28 +0200

    Indenting

--- src/components/user/Faves.jsx
+++ src/components/user/Faves.jsx
@@ -1,73 +1,75 @@
 import React from 'react';
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Faves.css';
 import PhotoBox from './Search/PhotoBox';
 
-function Faves(){ 
-  const [accessToken] = useState(localStorage.getItem("accessToken"));
-  if (accessToken === null) {
-      localStorage.clear();
-      window.location.href = "/login";
-  };
-  const userData = JSON.parse(localStorage.getItem("userData")) 
-  const userID = userData.user._id
-  console.log(userID);
-  const [faves,setFaves]=useState([]);
-  
-
-  const getAllFaves = ()=>{
-      axios.defaults.baseURL = "https://qasaqees.tech/api";
-      return (axios.get('/user/fav/'+userID)
-      .then((response) => {
-          const allFaves = response.data.favorites;
-          setFaves(allFaves);
-          console.log(allFaves)
-         
-      })
-      .catch((error) => {
-         if (error.response.status === 404) {
-            console.log(error.response.data.message);
-            
-        }
-    }));
-  }
-  
-      useEffect(() => {getAllFaves();}, []);
+function Faves() {
+    const [accessToken] = useState(localStorage.getItem("accessToken"));
+    if (accessToken === null) {
+        localStorage.clear();
+        window.location.href = "/login";
+    };
+    const userData = JSON.parse(localStorage.getItem("userData"))
+    const userID = userData.user._id
+    console.log(userID);
+    const [faves, setFaves] = useState([]);
 
 
-  return ( 
-    <div id="faves" className="container-fluid"> 
-        
-        {(faves.length > 0)?(faves.map((fave,index)=>{return(<PhotoBox key={index} id={fave._id} 
-                     url = {fave.url} title = {fave.title} userName={fave.creator.firstName +" "+ fave.creator.lastName} 
-                     favNum={fave.favouriteCount} comNum={fave.commentsNum}/>);})):<div className="heading">
-        <h1>Start picking your faves. Just click on the star.</h1>
-        <p> Like something you see? Let the favegrapher know by clicking on the star icon. </p>
-        <a href='/explore'><button>Start exploring flicker</button></a>
-        <img src="https://combo.staticflickr.com/ap/build/images/emptypages/faves3.jpg" alt="Snow"/>
-        </div> }
-   {/*                    {<div className="heading">
+    const getAllFaves = () => {
+        axios.defaults.baseURL = "https://qasaqees.tech/api";
+        return (axios.get('/user/fav/' + userID)
+            .then((response) => {
+                const allFaves = response.data.favorites;
+                setFaves(allFaves);
+                console.log(allFaves)
+
+            })
+            .catch((error) => {
+                if (error.response.status === 404) {
+                    console.log(error.response.data.message);
+
+                }
+            }));
+    }
+
+    useEffect(() => { getAllFaves(); }, []);
+
+
+    return (
+        <div id="faves" className="container-fluid">
+
+            {(faves.length > 0) ? (faves.map((fave, index) => {
+                return (<PhotoBox key={index} id={fave._id}
+                    url={fave.url} title={fave.title} userName={fave.creator.firstName + " " + fave.creator.lastName}
+                    favNum={fave.favouriteCount} comNum={fave.commentsNum} />);
+            })) : <div className="heading">
+                <h1>Start picking your faves. Just click on the star.</h1>
+                <p> Like something you see? Let the favegrapher know by clicking on the star icon. </p>
+                <a href='/explore'><button>Start exploring flicker</button></a>
+                <img src="https://combo.staticflickr.com/ap/build/images/emptypages/faves3.jpg" alt="Snow" />
+            </div>}
+            {/*                    {<div className="heading">
         <h1>Start picking your faves. Just click on the star.</h1>
         <p> Like something you see? Let the favegrapher know by clicking on the star icon. </p>
         <a href='/explore'><button>Start exploring flicker</button></a>
         <img src="https://combo.staticflickr.com/ap/build/images/emptypages/faves3.jpg" alt="Snow"/>
         </div>} */}
-          
-          
-
-
-    </div>
-    
-    
-   
 
 
 
-  );
+
+        </div>
 
 
-   
+
+
+
+
+    );
+
+
+
 
 
 }
--- src/components/user/ViewFollowers.jsx
+++ src/components/user/ViewFollowers.jsx
@@ -1,95 +1,95 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './ViewFollowers.css';
 
 function ViewFollowers() {
-                    
-  const [accessToken] = useState(localStorage.getItem("accessToken"));
-    if (accessToken === null) {
-        localStorage.clear();
-        window.location.href = "/login";
-    };
 
-  const userData = JSON.parse(localStorage.getItem("userData")) 
+  const [accessToken] = useState(localStorage.getItem("accessToken"));
+  if (accessToken === null) {
+    localStorage.clear();
+    window.location.href = "/login";
+  };
+
+  const userData = JSON.parse(localStorage.getItem("userData"))
   const userID = userData.user._id
   console.log(userID);
-  
-  const [followers,setFollowers]=useState([]);
-  const getAllFollowers = ()=>{
-      axios.defaults.baseURL = "https://qasaqees.tech/api";
-      axios.defaults.headers.common['Authorization'] = 'Bearer ' + accessToken;
-      return (axios.get('/user/followers/'+userID)
+
+  const [followers, setFollowers] = useState([]);
+  const getAllFollowers = () => {
+    axios.defaults.baseURL = "https://qasaqees.tech/api";
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + accessToken;
+    return (axios.get('/user/followers/' + userID)
       .then((response) => {
-          const allFollowers = response.data.followers;
-          setFollowers(allFollowers);
-          console.log(allFollowers)
-         
+        const allFollowers = response.data.followers;
+        setFollowers(allFollowers);
+        console.log(allFollowers)
+
       })
       .catch((error) => {
-         if (error.response.status === 404) {
-            console.log(error.response.data.message);
-            
-        }
-    }));
-  }
-  
-      useEffect(() => {getAllFollowers();}, []);
-return ( 
-<div id="fBody" className="container-fluid">
-  <table width="800" cellSpacing="0" id="SubNav">
-	  <tbody>
-      <tr>
-	      <td className="Buddy">
-	        <img src="https://combo.staticflickr.com/pw/images/buddyicon04.png#192776628@N07" alt="to your photostream page" width="48" height="48" className="personmenu-trigger"/>	
-	      </td>
-	        <td className="Section">
-	          <h1>People you follow</h1>
-	            <p id="contacts-subnav" className="LinksNewP">
-	              <span className="LinksNew">
-                  <span><a href=" ">Photos from</a></span> <span><a href=" ">Photos of</a></span><span className="Here">List</span><span><a href=" ">Find Friends</a></span>										<a href="/">Send Invites</a></span>	</p>
-          </td>       
-	    </tr>
-    </tbody>
-  </table>
-    <div className="contact-list-header contact-list-header-rev">
-      <form>
-        <span><strong>Show: </strong></span>
-          <select name="see">
-	          <option> all of your followers </option>
-          </select>
-              <span> or <a href={"/people/"+userID+"/contact/"}>See people you follow</a></span>                  
-      </form>  
-                                          
+        if (error.response.status === 404) {
+          console.log(error.response.data.message);
 
-    </div>
-       {(followers.length > 0) ?followers.map((follower , index) => {
+        }
+      }));
+  }
+
+  useEffect(() => { getAllFollowers(); }, []);
+  return (
+    <div id="fBody" className="container-fluid">
+      <table width="800" cellSpacing="0" id="SubNav">
+        <tbody>
+          <tr>
+            <td className="Buddy">
+              <img src="https://combo.staticflickr.com/pw/images/buddyicon04.png#192776628@N07" alt="to your photostream page" width="48" height="48" className="personmenu-trigger" />
+            </td>
+            <td className="Section">
+              <h1>People you follow</h1>
+              <p id="contacts-subnav" className="LinksNewP">
+                <span className="LinksNew">
+                  <span><a href=" ">Photos from</a></span> <span><a href=" ">Photos of</a></span><span className="Here">List</span><span><a href=" ">Find Friends</a></span>										<a href="/">Send Invites</a></span>	</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="contact-list-header contact-list-header-rev">
+        <form>
+          <span><strong>Show: </strong></span>
+          <select name="see">
+            <option> all of your followers </option>
+          </select>
+          <span> or <a href={"/people/" + userID + "/contact/"}>See people you follow</a></span>
+        </form>
+
+
+      </div>
+      {(followers.length > 0) ? followers.map((follower, index) => {
         return (<div key={index}>
           <table className="table">
-  <thead>
-    <tr>
-		<th></th>
-		<th>First Name</th>
-		<th>Last Name</th>
-    <th>Public Photos</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td><img src={follower.profilePhotoUrl} alt="public"
-        width="30"
-        length="40"
-      /></td>
-      <td>{follower.firstName}</td>
-      <td>{follower.lastName}</td>
-      <td>{follower.numberOfPhotos}</td>
-    </tr>
-	</tbody>
-	</table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Public Photos</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><img src={follower.profilePhotoUrl} alt="public"
+                  width="30"
+                  length="40"
+                /></td>
+                <td>{follower.firstName}</td>
+                <td>{follower.lastName}</td>
+                <td>{follower.numberOfPhotos}</td>
+              </tr>
+            </tbody>
+          </table>
 
 
 
-        
+
           {/*   <p> {follower._id}</p>
             <p> {follower.firstName}</p>
             <p> {follower.lastName}  </p> */}
@@ -98,17 +98,17 @@ return (
         </div>)
 
 
-      }): 	    <div className="ThinCase">
+      }) : <div className="ThinCase">
         <br />
-	      <h2 align="center" >No-one has listed you as a contact. <b><i>Yet</i></b>.</h2>
-                          <div></div>
-      </div>} 
+        <h2 align="center" >No-one has listed you as a contact. <b><i>Yet</i></b>.</h2>
+        <div></div>
+      </div>}
 
-</div>
+    </div>
 
 
 
-);
+  );
 
 
 
