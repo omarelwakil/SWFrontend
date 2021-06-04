e6acba40f8b7ec724cdbb18daf1c679c3ccebf40
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2021-06-04 18:41:45 +0200

    connected Delete Comment to Api

--- src/components/media/Comment.jsx
+++ src/components/media/Comment.jsx
@@ -11,8 +11,8 @@ function Comment(probs){//props:{id,creatorId img, firstName, lastName, date, te
         <div class="d-flex flex-row comment-row">
             <div class="p-2"><span class="round"><img src={probs.img} width="32" height="32" alt="user" /></span></div>
                 <div class="comment-text w-100">
-                    <h5 className="comment-author"><a className="comment-author">{probs.firstName} {probs.lastName}</a> <span class="comment-date">{probs.date}</span>
-                    {(deleteBtn)&&(<button className="delete-comment-btn"><i class="far fa-trash-alt"></i></button>)}
+                    <h5 className="comment-author"><a href={`/photos/${probs.creatorId}`} className="comment-author">{probs.firstName} {probs.lastName}</a> <span class="comment-date">{probs.date}</span>
+                    {(deleteBtn)&&(<button className="delete-comment-btn" onClick={(e) => probs.handleDelete(e,probs.commentId)}><i class="far fa-trash-alt"></i></button>)}
                     </h5>
                 <p class="comment-content">{probs.text}</p>
             </div>
