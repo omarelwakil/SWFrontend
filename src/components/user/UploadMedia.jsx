b58c8eb8cd765a6c535542062b478bb046e01091
Omar Elwakil <omartelwakil@gmail.com>
2021-06-07 13:20:05 +0200

    added toast to several components

--- src/components/user/UploadMedia.jsx
+++ src/components/user/UploadMedia.jsx
@@ -248,6 +248,23 @@ function UploadMedia() {
                         localStorage.clear();
                         window.location.href = "/login";
                     } else if (error.response.status === 400) {
+                        toastr.options = {
+                            "closeButton": true,
+                            "debug": false,
+                            "newestOnTop": true,
+                            "progressBar": true,
+                            "positionClass": "toast-top-center",
+                            "preventDuplicates": false,
+                            "onclick": null,
+                            "showDuration": "300",
+                            "hideDuration": "1000",
+                            "timeOut": "5000",
+                            "extendedTimeOut": "1000",
+                            "showEasing": "swing",
+                            "hideEasing": "linear",
+                            "showMethod": "fadeIn",
+                            "hideMethod": "fadeOut"
+                        };
                         toastr.error(error.response.data.message);
                     }
                 });
