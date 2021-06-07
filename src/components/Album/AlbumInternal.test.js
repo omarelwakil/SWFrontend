c5d509043df58471e4fb1772f3a9b8ff81162759
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2021-06-07 02:51:09 +0200

    added addtional unit testing

--- src/components/Album/AlbumInternal.test.js
+++ src/components/Album/AlbumInternal.test.js
@@ -10,7 +10,8 @@ configure({ adapter: new Adapter() });
 describe ('AlbumInternal', () => {
     it('Should render all images that will come from the backend', () => {
         const wrapper = shallow (<AlbumInternal 
-                                    albumId="1"
+                                    albumId="60baabf254a4dd00128e1c1f"
+                                    userId="60baab7c54a4dd00128e1c1e"
                                    />);
         expect(wrapper.find(Image)).not.toBeNull();
     });
