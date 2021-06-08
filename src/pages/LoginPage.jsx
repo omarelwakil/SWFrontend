39492ca8160c299afed84ba79230480824616bb4
Mousa Mohammad Abdulmaxod <49567708+MousaMohammad@users.noreply.github.com>
2021-06-08 07:21:49 +0200

    updated Documentation for Login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,7 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import UserlessNaviagationBar from '../components/user/UserlessNavigationBar';
 import Login from '../components/user/Login';
 import Footer from '../components/static/Footer';
-
+/**
+ * Component LoginPage for showing Login page 
+ *
+ * @component
+ * @example
+ * return(
+ *    <LoginPage />
+ * )
+ */
 function LoginPage() {
     return ( 
     <BrowserRouter>
